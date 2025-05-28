f80b850b57d13e621e55f049827d73ddec23c383
keramicheskiy <Sweetie.77@mail.ru>
2025-05-28 19:04:38 +0300

    fix main page

--- frontend/apps/users/views.py
+++ frontend/apps/users/views.py
@@ -7,12 +7,12 @@ from apps.authentication.utils import get_user
 @authenticated()
 def profile(request):
     user = get_user(request)
-    return redirect(to='/users/{}')
-
-
-    return render(request, "", context={"user": request.user})
+    return redirect(to=f'/users/{user.id}')
+    #
+    #
+    # return render(request, "", context={"user": request.user})
 
 @authenticated()
 def user_profile(request):
-
+    pass
 

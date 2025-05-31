5b76cff9aeec64a831c144be86a842343f1cb14e
keramicheskiy <Sweetie.77@mail.ru>
2025-05-31 08:43:29 +0300

    add a lot to frontend

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -102,8 +102,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/audience-allocation/'
-STATIC_ROOT = BASE_DIR / 'static' / 'audience-allocation'
+# STATIC_URL = '/static/audience-allocation/'
+# STATIC_ROOT = BASE_DIR / 'static' / 'audience-allocation'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

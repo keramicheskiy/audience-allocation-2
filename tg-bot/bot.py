78298e382cdb9084a67ed395c36bd48b10253786
keramicheskiy <Sweetie.77@mail.ru>
2025-06-01 10:04:15 +0300

    add a lot to the frontend

--- tg-bot/bot.py
+++ tg-bot/bot.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReactionTypeEmoji
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, ReactionTypeEmoji, InlineKeyboardMarkup, \
+    InlineKeyboardButton
 from urllib3 import request
 
 import services
@@ -15,52 +16,19 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    user_id = message.from_user.id
-    token = services.get_token(user_id)
-    if not token:
-        bot.reply_to(message, basic_start_message)
-        return
-    response = requests.get(url=BACKEND_URL + "/authentication/verify-token", cookies={"Token": token})
-    if response.status_code == 200:
-        role = response.json()["role"]
-        if role == "teacher":
-            bot.reply_to(message, teacher_start_message)
-            return
-        elif role == "moderator":
-            bot.reply_to(message, moderator_start_message)
-            return
-    bot.reply_to(message, f"Ошибка {response.status_code}")
+    markup = InlineKeyboardMarkup()
+    url = "https://krmch.ru/"
+    markup.add(InlineKeyboardButton("Открыть мини-приложение", url=url))
 
-
-@bot.message_handler(commands=['register'])
-def register(message):
-    bot.reply_to(message, 'Дароу')
-
-
-@bot.message_handler(commands=['login'])
-def send_welcome(message):
-    bot.reply_to(message, 'Дароу')
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Дароу')
-
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, 'Дароу')
-
-
-@bot.message_handler()
-def text_handler(message):
-    pass
+    bot.send_message(
+        message.chat.id,
+        "Нажмите на кнопку ниже, чтобы открыть мини-приложение.",
+        reply_markup=markup
+    )
 
 
 if __name__ == "__main__":
     print("Бот запущен...")
     services.create_table()
-    # bot.add_edited_message_handler({
-    #     'callback': handle_edited_message,
-    # })
+
     bot.polling(none_stop=True)
